3ef312c12de8e1d26376e6d2b419a7534f4e56e1
emelicat <87022070+emelicat@users.noreply.github.com>
2024-11-19 12:39:20 +0000

    0839

--- lesson9/lesson9_2.py
+++ lesson9/lesson9_2.py
@@ -1,8 +1,9 @@
-import tools
+import gear
+import gear.widget
 if __name__ == '__main__':
  nums = int(input('請輸入學生數量:'))
- student_names:list[str] = tools.get_names(nums=nums)
- students = tools.generate_students(names=student_names)
+ student_names:list[str] = gear.widget.get_names(nums=nums)
+ students = gear.widget.generate_students(names=student_names)
  for name in students:
     for key,value in name.items():
         print(f'{key}:{value}')
